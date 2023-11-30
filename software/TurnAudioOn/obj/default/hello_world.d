3793af05c020f0bbf30f770eb34f429753a60e11
raivo353 <91531605+raivo353@users.noreply.github.com>
2023-11-30 11:41:37 +0100

    Audio device opened

---
+++ software/TurnAudioOn/obj/default/hello_world.d
@@ -0,0 +1,20 @@
+obj/default/hello_world.o: hello_world.c \
+ ../TurnAudioOn_bsp//drivers/inc/altera_up_avalon_audio.h \
+ ../TurnAudioOn_bsp//HAL/inc/alt_types.h \
+ ../TurnAudioOn_bsp//HAL/inc/sys/alt_dev.h ../TurnAudioOn_bsp/system.h \
+ ../TurnAudioOn_bsp/linker.h ../TurnAudioOn_bsp//HAL/inc/sys/alt_llist.h \
+ ../TurnAudioOn_bsp//HAL/inc/priv/alt_dev_llist.h
+
+../TurnAudioOn_bsp//drivers/inc/altera_up_avalon_audio.h:
+
+../TurnAudioOn_bsp//HAL/inc/alt_types.h:
+
+../TurnAudioOn_bsp//HAL/inc/sys/alt_dev.h:
+
+../TurnAudioOn_bsp/system.h:
+
+../TurnAudioOn_bsp/linker.h:
+
+../TurnAudioOn_bsp//HAL/inc/sys/alt_llist.h:
+
+../TurnAudioOn_bsp//HAL/inc/priv/alt_dev_llist.h:
