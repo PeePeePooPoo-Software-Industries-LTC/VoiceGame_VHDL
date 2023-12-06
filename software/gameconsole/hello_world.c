98baa5676a461c01cc6e0c76d0b8ecbb30bdf52b
jaschutte <34577095+jaschutte@users.noreply.github.com>
2023-12-06 17:16:00 +0100

    A form of a framebuffer

--- software/gameconsole/hello_world.c
+++ software/gameconsole/hello_world.c
@@ -5,42 +5,48 @@
 #include "io.h"
 #include "sys/alt_irq.h"
 
-#define WRITE_SDRAM(offset, value) *(NEW_SDRAM_CONTROLLER_0_BASE + offset) = value;
-#define READ_SDRAM(offset) (*(NEW_SDRAM_CONTROLLER_0_BASE + offset))
+//#define WRITE_SDRAM(offset, value) IOWR(NEW_SDRAM_CONTROLLER_0_BASE, offset, value);
+//#define READ_SDRAM(offset) IORD(NEW_SDRAM_CONTROLLER_0_BASE, offset)
 
-void return_color_irq(void* irq_context, long unsigned int irq) {
-	if (irq == PIO_REQUEST_IRQ) {
-		IOWR(PIO_PIXEL_COLOR_BASE, 0, 0x00ffff0f);
-	}
+#define WRITE_SDRAM(offset, value) *(int*)(NEW_SDRAM_CONTROLLER_0_BASE + offset) = value;
+#define READ_SDRAM(offset) (*(int*)NEW_SDRAM_CONTROLLER_0_BASE + offset)
+
+#define VGA_WIDTH 640
+#define VGA_HEIGHT 480
+
+#define RGB(r, g, b) ((r << 16) | (g << 8) | b)
+
+void update_screen();
+void set_pixel_color(int x, int y, int color);
+
+inline void set_pixel_color(int x, int y, int color) {
+	WRITE_SDRAM(y * VGA_HEIGHT + x, color);
+}
+
+inline void update_screen() {
+	int position = IORD(PIO_PIXEL_POSITION_BASE, 0);
+//	int x = (position >> 16);
+//	int y = position & 0xffff;
+
+	IOWR(PIO_PIXEL_COLOR_BASE, 0, position);
+//	IOWR(PIO_PIXEL_COLOR_BASE, 0, RGB(x, 0, 0));
+
+//	IOWR(PIO_PIXEL_COLOR_BASE, 0, READ_SDRAM(y * VGA_HEIGHT + x));
 }
 
 int main() {
-	alt_irq_init(0);
-
-	alt_irq_register(
-		PIO_REQUEST_IRQ,
-		0,
-		return_color_irq
-	);
-
-	int position = IORD(PIO_PIXEL_POSITION_BASE, 0);
-	printf("(%u, %u)\n", position >> 16, position & 0xffff);
-	IOWR(PIO_PIXEL_COLOR_BASE, 0, 0x00ffff03);
-	printf("why\n");
-
-	int prev_value = IORD(PIO_REQUEST_BASE, 0);
-	while (1) {
-		int current_value = IORD(PIO_REQUEST_BASE, 0);
-
-//		printf("%i\n", current_value);
-
-//		if (current_value == 1) {
-//			printf("You should INTERRUPT yourself NOW!\n");
-//		}
-		if (prev_value != current_value) {
-			prev_value = current_value;
-			return_color_irq(0, PIO_REQUEST_IRQ);
-			printf("Current: %i\n", current_value);
+	for (int x = 0; x < VGA_WIDTH; x++) {
+		for (int y = 0; y < VGA_HEIGHT; y++) {
+//			if (x < 10) {
+//				set_pixel_color(x, y, 0x00ff0000);
+//			} else {
+				set_pixel_color(x, y, 0x000000ff);
+//			}
 		}
+	}
+
+	while (1) {
+		IOWR(PIO_PIXEL_COLOR_BASE, 0, IORD(PIO_PIXEL_POSITION_BASE, 0));
+//		update_screen();
 	}
 }
