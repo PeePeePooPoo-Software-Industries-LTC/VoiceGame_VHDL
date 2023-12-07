d4a3b3c61d7b5349fc14b33650998e3f901fafbb
jaschutte <34577095+jaschutte@users.noreply.github.com>
2023-12-07 16:14:24 +0100

    Played around with backbuffer, possibly switching back to SDRAM

--- software/gameconsole/hello_world.c
+++ software/gameconsole/hello_world.c
@@ -8,18 +8,53 @@
 #define RGB(r, g, b) (((r) << 20) | ((g) << 10) | (b))
 #define BIT10_MAX (0x3ff)
 
-int main() {
+#define SCALE(n, input_max, output_max) (n * input_max / output_max)
 
-	alt_up_pixel_buffer_dma_dev* pixel_buffer = alt_up_pixel_buffer_dma_open_dev(VIDEO_PIXEL_BUFFER_DMA_0_NAME);
-	if (pixel_buffer == NULL) {
+typedef int Color;
+typedef struct VgaBuffer_t {
+	alt_up_pixel_buffer_dma_dev* device;
+	unsigned int current_buffer;
+} VgaBuffer;
+
+void vga_swap_buffers(VgaBuffer* buff);
+void vga_clear(VgaBuffer* buff);
+void vga_draw_pixel(VgaBuffer* buff, int x, int y, Color);
+void vga_draw_rect(VgaBuffer* buff, int x, int y, int w, int h, Color);
+
+inline void vga_swap_buffers(VgaBuffer* buff) {
+	buff->current_buffer = !(buff->current_buffer);
+	alt_up_pixel_buffer_dma_swap_buffers(buff->device);
+}
+
+inline void vga_draw_pixel(VgaBuffer* buff, int x, int y, Color color) {
+	alt_up_pixel_buffer_dma_draw(buff->device, x, y, color);
+}
+
+inline void vga_clear(VgaBuffer* buff) {
+	alt_up_pixel_buffer_dma_clear_screen(buff->device, buff->current_buffer);
+}
+
+inline void vga_draw_rect(VgaBuffer* buff, int x, int y, int w, int h, Color color) {
+	alt_up_pixel_buffer_dma_draw_box(buff->device, x, y, x + w, y + h, color, buff->current_buffer);
+}
+
+void draw() {
+
+}
+
+int main() {
+	VgaBuffer vga_buffer = {
+		alt_up_pixel_buffer_dma_open_dev(VIDEO_PIXEL_BUFFER_DMA_0_NAME),
+		0
+	};
+	if (vga_buffer.device == NULL) {
 		printf("Failed to open device\n");
-	} else {
-		printf("I am a god amongst men\n");
+		return 1;
 	}
 
-	alt_up_pixel_buffer_dma_clear_screen(pixel_buffer, 0);
-	alt_up_pixel_buffer_dma_draw(pixel_buffer, 0xffffffff, 5, 5);
-	alt_up_pixel_buffer_dma_draw(pixel_buffer, 0xffffffff, 15, 5);
+	printf("Addresses (FRONT: %p) (BACK: %p)", vga_buffer.device->buffer_start_address, vga_buffer.device->back_buffer_start_address);
+
+	vga_clear(&vga_buffer);
 
 	int x = 0;
 	while (1) {
@@ -32,8 +67,10 @@ int main() {
 
 		int normalized = x * BIT10_MAX / 620;
 
-		alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x + 20, 220, RGB(normalized, BIT10_MAX - normalized, 0), 0);
+		vga_draw_rect(&vga_buffer, x, 200, 20, 20, RGB(normalized, BIT10_MAX - normalized, 0));
 
+		vga_swap_buffers(&vga_buffer);
 		usleep(16000);
 	}
+	return 0;
 }
