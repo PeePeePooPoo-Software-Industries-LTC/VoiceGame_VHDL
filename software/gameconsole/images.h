8559bbd6168fe397bd21a1a7a8256745457a0f3f
jaschutte <34577095+jaschutte@users.noreply.github.com>
2023-12-19 15:00:09 +0100

    Added images

---
+++ software/gameconsole/images.h
@@ -0,0 +1,144 @@
+// AUTO-GENERATED HEADER FILE CONTENT
+#ifndef __IMAGES_H_FILE__
+#define __IMAGES_H_FILE__
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_tail_west.png
+#define IMAGE_snake_tail_west_MAX_BYTES 62
+extern unsigned char image_snake_tail_west_width;
+extern unsigned char image_snake_tail_west_height;
+extern unsigned int image_snake_tail_west_palette[8];
+extern unsigned char image_snake_tail_west[IMAGE_snake_tail_west_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_tail_south.png
+#define IMAGE_snake_tail_south_MAX_BYTES 65
+extern unsigned char image_snake_tail_south_width;
+extern unsigned char image_snake_tail_south_height;
+extern unsigned int image_snake_tail_south_palette[8];
+extern unsigned char image_snake_tail_south[IMAGE_snake_tail_south_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_tail_north.png
+#define IMAGE_snake_tail_north_MAX_BYTES 65
+extern unsigned char image_snake_tail_north_width;
+extern unsigned char image_snake_tail_north_height;
+extern unsigned int image_snake_tail_north_palette[8];
+extern unsigned char image_snake_tail_north[IMAGE_snake_tail_north_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_tail_east.png
+#define IMAGE_snake_tail_east_MAX_BYTES 62
+extern unsigned char image_snake_tail_east_width;
+extern unsigned char image_snake_tail_east_height;
+extern unsigned int image_snake_tail_east_palette[8];
+extern unsigned char image_snake_tail_east[IMAGE_snake_tail_east_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_headed_west.png
+#define IMAGE_snake_headed_west_MAX_BYTES 66
+extern unsigned char image_snake_headed_west_width;
+extern unsigned char image_snake_headed_west_height;
+extern unsigned int image_snake_headed_west_palette[8];
+extern unsigned char image_snake_headed_west[IMAGE_snake_headed_west_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_headed_south.png
+#define IMAGE_snake_headed_south_MAX_BYTES 66
+extern unsigned char image_snake_headed_south_width;
+extern unsigned char image_snake_headed_south_height;
+extern unsigned int image_snake_headed_south_palette[8];
+extern unsigned char image_snake_headed_south[IMAGE_snake_headed_south_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_headed_north.png
+#define IMAGE_snake_headed_north_MAX_BYTES 66
+extern unsigned char image_snake_headed_north_width;
+extern unsigned char image_snake_headed_north_height;
+extern unsigned int image_snake_headed_north_palette[8];
+extern unsigned char image_snake_headed_north[IMAGE_snake_headed_north_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_headed_east.png
+#define IMAGE_snake_headed_east_MAX_BYTES 66
+extern unsigned char image_snake_headed_east_width;
+extern unsigned char image_snake_headed_east_height;
+extern unsigned int image_snake_headed_east_palette[8];
+extern unsigned char image_snake_headed_east[IMAGE_snake_headed_east_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_corner_ws.png
+#define IMAGE_snake_corner_ws_MAX_BYTES 99
+extern unsigned char image_snake_corner_ws_width;
+extern unsigned char image_snake_corner_ws_height;
+extern unsigned int image_snake_corner_ws_palette[8];
+extern unsigned char image_snake_corner_ws[IMAGE_snake_corner_ws_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_corner_wn.png
+#define IMAGE_snake_corner_wn_MAX_BYTES 93
+extern unsigned char image_snake_corner_wn_width;
+extern unsigned char image_snake_corner_wn_height;
+extern unsigned int image_snake_corner_wn_palette[8];
+extern unsigned char image_snake_corner_wn[IMAGE_snake_corner_wn_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_corner_sw.png
+#define IMAGE_snake_corner_sw_MAX_BYTES 94
+extern unsigned char image_snake_corner_sw_width;
+extern unsigned char image_snake_corner_sw_height;
+extern unsigned int image_snake_corner_sw_palette[8];
+extern unsigned char image_snake_corner_sw[IMAGE_snake_corner_sw_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_corner_se.png
+#define IMAGE_snake_corner_se_MAX_BYTES 90
+extern unsigned char image_snake_corner_se_width;
+extern unsigned char image_snake_corner_se_height;
+extern unsigned int image_snake_corner_se_palette[8];
+extern unsigned char image_snake_corner_se[IMAGE_snake_corner_se_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_corner_nw.png
+#define IMAGE_snake_corner_nw_MAX_BYTES 90
+extern unsigned char image_snake_corner_nw_width;
+extern unsigned char image_snake_corner_nw_height;
+extern unsigned int image_snake_corner_nw_palette[8];
+extern unsigned char image_snake_corner_nw[IMAGE_snake_corner_nw_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_corner_ne.png
+#define IMAGE_snake_corner_ne_MAX_BYTES 92
+extern unsigned char image_snake_corner_ne_width;
+extern unsigned char image_snake_corner_ne_height;
+extern unsigned int image_snake_corner_ne_palette[8];
+extern unsigned char image_snake_corner_ne[IMAGE_snake_corner_ne_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_corner_es.png
+#define IMAGE_snake_corner_es_MAX_BYTES 95
+extern unsigned char image_snake_corner_es_width;
+extern unsigned char image_snake_corner_es_height;
+extern unsigned int image_snake_corner_es_palette[8];
+extern unsigned char image_snake_corner_es[IMAGE_snake_corner_es_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_corner_en.png
+#define IMAGE_snake_corner_en_MAX_BYTES 99
+extern unsigned char image_snake_corner_en_width;
+extern unsigned char image_snake_corner_en_height;
+extern unsigned int image_snake_corner_en_palette[8];
+extern unsigned char image_snake_corner_en[IMAGE_snake_corner_en_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_body_west.png
+#define IMAGE_snake_body_west_MAX_BYTES 113
+extern unsigned char image_snake_body_west_width;
+extern unsigned char image_snake_body_west_height;
+extern unsigned int image_snake_body_west_palette[8];
+extern unsigned char image_snake_body_west[IMAGE_snake_body_west_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_body_south.png
+#define IMAGE_snake_body_south_MAX_BYTES 57
+extern unsigned char image_snake_body_south_width;
+extern unsigned char image_snake_body_south_height;
+extern unsigned int image_snake_body_south_palette[8];
+extern unsigned char image_snake_body_south[IMAGE_snake_body_south_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_body_north.png
+#define IMAGE_snake_body_north_MAX_BYTES 57
+extern unsigned char image_snake_body_north_width;
+extern unsigned char image_snake_body_north_height;
+extern unsigned int image_snake_body_north_palette[8];
+extern unsigned char image_snake_body_north[IMAGE_snake_body_north_MAX_BYTES];
+
+// AUTO-GENERATED IMAGE CONVERTED FROM: snake_body_east.png
+#define IMAGE_snake_body_east_MAX_BYTES 113
+extern unsigned char image_snake_body_east_width;
+extern unsigned char image_snake_body_east_height;
+extern unsigned int image_snake_body_east_palette[8];
+extern unsigned char image_snake_body_east[IMAGE_snake_body_east_MAX_BYTES];
+#endif
