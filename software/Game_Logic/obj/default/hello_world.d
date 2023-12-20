dcc0a02198863d2972646818cf732f828d1801ae
raivo353 <91531605+raivo353@users.noreply.github.com>
2023-12-20 13:36:44 +0100

    game over screen inclusief bugs

---
+++ software/Game_Logic/obj/default/hello_world.d
@@ -0,0 +1,34 @@
+obj/default/hello_world.o: hello_world.c ../Game_Logic_bsp/system.h \
+ ../Game_Logic_bsp/linker.h ../Game_Logic_bsp//HAL/inc/sys/alt_irq.h \
+ ../Game_Logic_bsp//HAL/inc/nios2.h \
+ ../Game_Logic_bsp//HAL/inc/alt_types.h \
+ ../Game_Logic_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../Game_Logic_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
+ ../Game_Logic_bsp//HAL/inc/sys/alt_dev.h \
+ ../Game_Logic_bsp//HAL/inc/sys/alt_llist.h \
+ ../Game_Logic_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../Game_Logic_bsp//HAL/inc/io.h ../Game_Logic_bsp//HAL/inc/alt_types.h
+
+../Game_Logic_bsp/system.h:
+
+../Game_Logic_bsp/linker.h:
+
+../Game_Logic_bsp//HAL/inc/sys/alt_irq.h:
+
+../Game_Logic_bsp//HAL/inc/nios2.h:
+
+../Game_Logic_bsp//HAL/inc/alt_types.h:
+
+../Game_Logic_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../Game_Logic_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
+
+../Game_Logic_bsp//HAL/inc/sys/alt_dev.h:
+
+../Game_Logic_bsp//HAL/inc/sys/alt_llist.h:
+
+../Game_Logic_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../Game_Logic_bsp//HAL/inc/io.h:
+
+../Game_Logic_bsp//HAL/inc/alt_types.h:
